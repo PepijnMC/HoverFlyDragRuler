c7e664ea06d631f162fb8a27ec371966d75b1a6c
Pepijn <pepijnsietsema@gmail.com>
2022-01-16 00:10:12 +0100

    Update hook.js

--- scripts/hook.js
+++ scripts/hook.js
@@ -1,17 +1,16 @@
 Hooks.once("dragRuler.ready", (SpeedProvider) => {
-    class HoverFlySpeedProvider extends SpeedProvider {
+	class HoverFlySpeedProvider extends SpeedProvider {
 		get colors() {
 			return [
-				{id: "walk", default: 0x00FF00, name: "drag-ruler.genericSpeedProvider.speeds.walk"},
-				{id: "dash", default: 0xFFFF00, name: "drag-ruler.genericSpeedProvider.speeds.dash"}
+				{id: "walk", default: 0x00FF00, name: "drag-ruler.hover-fly-drag-ruler.speeds.walk"},
+				{id: "dash", default: 0xFFFF00, name: "drag-ruler.hover-fly-drag-ruler.speeds.dash"}
 			]
 		}
 	
 		getRanges(token) {
-			const speedAttribute = "actor.data.data.attributes.movement.walk";
-			if (!speedAttribute)
-				return []
-			const tokenSpeed = parseFloat(getProperty(token, speedAttribute));
+			const walkAttribute = parseFloat(getProperty(token, "actor.data.data.attributes.movement.walk"));
+			const flyAttribute = parseFloat(getProperty(token, "actor.data.data.attributes.movement.fly"));
+			const tokenSpeed = Math.max(walkAttribute, flyAttribute);
 			if (tokenSpeed === undefined) {
 				console.warn(`Drag Ruler (Generic Speed Provider) | The configured token speed attribute "${speedAttribute}" didn't return a speed value. To use colors based on drag distance set the setting to the correct value (or clear the box to disable this feature).`)
 				return []
@@ -23,7 +22,7 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
 		}
 	
 
-        getCostForStep(token, area, options={}) {
+		getCostForStep(token, area, options={}) {
 			// Lookup the cost for each square occupied by the token
 			options.token = token;
 			if (token.actor.data.data.attributes.movement["hover"] || token.actor.data.data.attributes.movement["fly"])
@@ -32,7 +31,7 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
 			// Return the maximum of the costs
 			return costs.reduce((max, current) => Math.max(max, current));
 		}
-    }
+	}
 
-    dragRuler.registerModule("hover-fly-drag-ruler", HoverFlySpeedProvider)
+	dragRuler.registerModule("hover-fly-drag-ruler", HoverFlySpeedProvider)
 })
