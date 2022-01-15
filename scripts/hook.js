9461123651d221053cf852937423e3049c540235
Pepijn <pepijnsietsema@gmail.com>
2022-01-15 22:04:13 +0100

    Create hook.js

---
+++ scripts/hook.js
@@ -0,0 +1,15 @@
+Hooks.once("dragRuler.ready", (SpeedProvider) => {
+    class HoverFlySpeedProvider extends SpeedProvider {
+        getCostForStep(token, area, options={}) {
+			// Lookup the cost for each square occupied by the token
+			options.token = token;
+			if (token.actor.data.data.attributes.movement["hover"] || token.actor.data.data.attributes.movement["fly"])
+				return 1;
+			const costs = area.map(space => terrainRuler.getCost(space.x, space.y, options));
+			// Return the maximum of the costs
+			return costs.reduce((max, current) => Math.max(max, current))
+		}
+    }
+
+    dragRuler.registerModule("my-module-id", HoverFlySpeedProvider)
+})
